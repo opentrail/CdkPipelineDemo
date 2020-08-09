e24de66de65e23bbe2038860c6ebd7f92509f4f9
Jonathan Greenwood <opentrail@gmail.com>
2020-08-09 12:44:45 +0200

    fixed filename typos

--- test/cdkpipelines-demo.test.ts
+++ test/cdkpipelines-demo.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CdkpipelineDemo from '../lib/cdkpipeline-demo-stack';
+import * as CdkpipelinesDemo from '../lib/cdkpipelines-demo-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkpipelineDemo.CdkpipelineDemoStack(app, 'MyTestStack');
+    const stack = new CdkpipelinesDemo.CdkpipelinesDemoStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
