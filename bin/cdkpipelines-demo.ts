a619428945a4601aa3100a5b5b7d5349710c0590
Jonathan Greenwood <opentrail@gmail.com>
2020-08-09 19:33:11 +0200

    Add dev env settings

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -2,10 +2,16 @@
 import 'source-map-support/register';
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoStage } from '../lib/cdkpipelines-demo-stage';
 
 const app = new App();
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
     env: { account: '035651948618', region: 'eu-west-2' },
 });
 
+/**
+* new CdkpipelinesDemoStage(app, 'Dev', {
+*    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+* });
+*/
 app.synth();
